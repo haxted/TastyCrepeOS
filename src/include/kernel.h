e57a813492806f2cf67818f64fc383d53756145f
haxted <vlorpprolpor@gmail.com>
2025-04-12 22:10:57 +0200

    Update kernel.h

--- src/include/kernel.h
+++ src/include/kernel.h
@@ -1,7 +1,7 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 #define TC_VER "0.6a"
-#define TC_BUILD "15250"
+#define TC_BUILD "16252"
 #define TC_MOTB "Making square wheels to better admire the round ones"
 
 void kmain(void);
