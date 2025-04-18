5ee2748e5c73ec15c3fc62ecf8f73314f957bdad
haxted <vlorpprolpor@gmail.com>
2025-04-18 18:20:59 +0200

    Update kernel.h

--- src/include/kernel.h
+++ src/include/kernel.h
@@ -1,8 +1,8 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 #define TC_VER "0.7-1a"
-#define TC_BUILD "16254"
-#define TC_MOTB "Windows incoming..."
+#define TC_BUILD "18252"
+#define TC_MOTB "1 pro, 1006 cons"
 
 void kmain(void);
 void panic(const char *r);
