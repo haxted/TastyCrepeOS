18dcab26989c9a305285def4737a869cc1bc8762
haxted <vlorpprolpor@gmail.com>
2025-04-20 20:04:26 +0200

    Update kernel.h (literally)

--- src/include/kernel.h
+++ src/include/kernel.h
@@ -1,8 +1,8 @@
 #ifndef KERNEL_H
 #define KERNEL_H
-#define TC_VER "0.8.4a"
-#define TC_BUILD "18252"
-#define TC_MOTB "1 pro, 1006 cons"
+#define TC_VER "0.9.0a"
+#define TC_BUILD "17253"
+#define TC_MOTB "if(errors) return toBed;"
 
 void kmain(void);
 void panic(const char *r);
